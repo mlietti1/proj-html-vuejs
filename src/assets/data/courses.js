859d68420a64eb3def4a34e4f5e67283f826bc20
mlietti1 <mcf.lietti@outlook.com>
2022-11-28 17:21:52 +0100

    slider in main

---
+++ src/assets/data/courses.js
@@ -0,0 +1,153 @@
+export default [
+  {
+    img: 'course-02-480x298.jpg',
+    price: '$40',
+    cents:'.00',
+    title: 'Learning to Write as a Professional Author',
+    lessons: 20,
+    students: 50,
+    free: false,
+    id: 1
+  },
+  {
+    img: 'stock-full-hd-03-480x298.jpg',
+    price: '$0',
+    cents:'.00',
+    title: 'Customer-centric Info-Tech Strategies',
+    lessons: 24,
+    students: 769,
+    free: true,
+    id: 2
+  },
+  {
+    img: 'stock-full-hd-04-480x298.jpg',
+    price: '$19',
+    cents:'.00',
+    title: 'Open Programming Courses for Everyone: Python',
+    lessons: 17,
+    students: 62,
+    free: false,
+    id: 3
+  },
+  {
+    img: 'course-02-480x298.jpg',
+    price: '$40',
+    cents:'.00',
+    title: 'Learning to Write as a Professional Author',
+    lessons: 20,
+    students: 50,
+    free: false,
+    id: 4
+  },
+  {
+    img: 'stock-full-hd-03-480x298.jpg',
+    price: '$0',
+    cents:'.00',
+    title: 'Customer-centric Info-Tech Strategies',
+    lessons: 24,
+    students: 769,
+    free: true,
+    id: 5
+  },
+  {
+    img: 'stock-full-hd-04-480x298.jpg',
+    price: '$19',
+    cents:'.00',
+    title: 'Open Programming Courses for Everyone: Python',
+    lessons: 17,
+    students: 62,
+    free: false,
+    id: 6
+  },
+  {
+    img: 'course-02-480x298.jpg',
+    price: '$40',
+    cents:'.00',
+    title: 'Learning to Write as a Professional Author',
+    lessons: 20,
+    students: 50,
+    free: false,
+    id: 7
+  },
+  {
+    img: 'stock-full-hd-03-480x298.jpg',
+    price: '$0',
+    cents:'.00',
+    title: 'Customer-centric Info-Tech Strategies',
+    lessons: 24,
+    students: 769,
+    free: true,
+    id: 8
+  },
+  {
+    img: 'stock-full-hd-04-480x298.jpg',
+    price: '$19',
+    cents:'.00',
+    title: 'Open Programming Courses for Everyone: Python',
+    lessons: 17,
+    students: 62,
+    free: false,
+    id: 9
+  },
+  {
+    img: 'course-02-480x298.jpg',
+    price: '$40',
+    cents:'.00',
+    title: 'Learning to Write as a Professional Author',
+    lessons: 20,
+    students: 50,
+    free: false,
+    id: 10
+  },
+  {
+    img: 'stock-full-hd-03-480x298.jpg',
+    price: '$0',
+    cents:'.00',
+    title: 'Customer-centric Info-Tech Strategies',
+    lessons: 24,
+    students: 769,
+    free: true,
+    id: 11
+  },
+  {
+    img: 'stock-full-hd-04-480x298.jpg',
+    price: '$19',
+    cents:'.00',
+    title: 'Open Programming Courses for Everyone: Python',
+    lessons: 17,
+    students: 62,
+    free: false,
+    id: 12
+  },
+  {
+    img: 'course-02-480x298.jpg',
+    price: '$40',
+    cents:'.00',
+    title: 'Learning to Write as a Professional Author',
+    lessons: 20,
+    students: 50,
+    free: false,
+    id: 13
+  },
+  {
+    img: 'stock-full-hd-03-480x298.jpg',
+    price: '$0',
+    cents:'.00',
+    title: 'Customer-centric Info-Tech Strategies',
+    lessons: 24,
+    students: 769,
+    free: true,
+    id: 14
+  },
+  {
+    img: 'stock-full-hd-04-480x298.jpg',
+    price: '$19',
+    cents:'.00',
+    title: 'Open Programming Courses for Everyone: Python',
+    lessons: 17,
+    students: 62,
+    free: false,
+    id: 15
+  }
+
+]
